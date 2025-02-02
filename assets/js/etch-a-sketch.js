bbedbfb008cad3bf5526c7e59201bb082d8b5f09
ebaek88 <93978710+ebaek88@users.noreply.github.com>
2025-02-01 19:51:09 -0700

    feat: add resetGrid(), randomColor, and opacity
    resetGrid() redraws the grid with different number
    of squares.
    
    Also added random coloring and increasing opacity
    features. Therefore, when the square is visited by
    the mouse cursor, the opacity of the corresponding
    div container will increase from 0.0 to 1.0.

--- assets/js/etch-a-sketch.js
+++ assets/js/etch-a-sketch.js
@@ -8,7 +8,7 @@ function drawGrid(squares = 4) {
     gridContainer.appendChild(rowContainer);
     
     // attach the "hover" event listener to each row
-    rowContainer.addEventListener("mousemove", changeColor, {capture: true});
+    rowContainer.addEventListener("mouseover", changeColor, {capture: true});
     
     for(let j = 0; j < squares; j++) {
       const squareContainer = document.createElement("div")
@@ -18,12 +18,54 @@ function drawGrid(squares = 4) {
   }
 }
 
+// change the color of each square div
 function changeColor(event) {
-  let target = event.target;
-  target.setAttribute("style", "background-color: #000");
+  event.stopPropagation();
+  const target = event.target;
+  const targetColorBackground = target.getAttribute("style");
+  let currentOpacity;
+
+  if(targetColorBackground) {
+    // need to trim the string that has the opacity value
+    // rgba() is now deprecated. We are using rgb( r g b / opacity) instead
+    const opacityString = targetColorBackground.slice( (targetColorBackground.indexOf("/") + 1) , (targetColorBackground.indexOf(")")) );
+    currentOpacity = Number.parseFloat(opacityString);
+    // console.log(currentOpacity);
+  }
+
+  const randomR = Math.floor(Math.random() * 255);
+  const randomG = Math.floor(Math.random() * 255);
+  const randomB = Math.floor(Math.random() * 255);
+  // when the grid is drawn in the beginning, the opacity is undefined
+  target.setAttribute("style", `background-color: rgb( ${randomR} ${randomG} ${randomB} / ${currentOpacity ? (currentOpacity.toFixed(1) === "1.0" ? currentOpacity : currentOpacity + 0.1) : 0.1 } )`);
+  // console.log(target.getAttribute("style"));
+}
+
+// clear the grid
+function clear() {
+  const gridContainer = document.querySelector(".grid");
+  while(gridContainer.firstChild) {
+    gridContainer.removeChild(gridContainer.firstChild);
+  }
+}
+
+// resets the grid when the button is clicked
+function resetGrid() {
+  const squaresPerSide = prompt("How many squares per side for the new grid? (maximum 100)");
+  const intSquare = Number.parseInt(squaresPerSide);
+  if ( !(intSquare >= 1 && intSquare <= 100) ) {
+    alert("Can't create a grid with the number entered. Please enter the appropriate number again.");
+    return;
+  }
+
+  clear();
+  drawGrid(intSquare);
 }
 
 function main() {
+  const buttonElement = document.querySelector(".reset");
+  buttonElement.addEventListener("click", resetGrid);
+
   // first start with 4 * 4 grid
   drawGrid();
 }
